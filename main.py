cf1fb5ad34a21fbb01ac317060ff212ebf0bbb4c
zengshuai <zengs1994@gmail.com>
2025-06-04 22:35:47 +0800

    init commit

---
+++ main.py
@@ -0,0 +1,41 @@
+import os
+import re
+
+import easyocr
+
+
+def get_image_files(dir: str) -> list[str]:
+    image_extensions = {".jpg", "jpeg", ".png", ".bmp", ".webp"}
+    return [
+        os.path.join(dir, f)
+        for f in os.listdir(dir)
+        if os.path.splitext(f)[1].lower() in image_extensions
+    ]
+
+
+def readTexts(image: str):
+    reader: easyocr.Reader = easyocr.Reader(["en"])
+    return reader.readtext(
+        image,
+        detail=0,
+    )
+
+
+image_dir = "./pictures/"
+codeList: list[str] = []
+pattern = re.compile("[a-zA-Z]{1,}[- ]{0,}[0-9]{1,}")
+
+for image in get_image_files(image_dir):
+    for text in readTexts(image):
+        match = pattern.search(str(text))
+        if match:
+            codeList.append(match.group())
+
+print("match code is", codeList)
+
+with open("codelist.txt", "w") as file:
+    for code in codeList:
+        file.write(code)
+        file.write("\n")
+
+print("code write to codelist done!")
